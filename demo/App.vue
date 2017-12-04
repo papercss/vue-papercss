2990d8198efd4e2f2f30d8acc69c1e0dd3df8f96
Thomas Cazade <cazade.thomas@gmail.com>
2017-12-04 12:13:33 +0100

    Added some example buttons

--- demo/App.vue
+++ demo/App.vue
@@ -1,9 +1,25 @@
 <template lang="html">
-	<h1>PaperCSS for VueJS</h1>
+
+	<div id="demo">
+		<h1>PaperCSS for VueJS</h1>
+
+		<div id="demo-content">
+			<p-button>Basic button</p-button>
+			<p-button :small="true">Small button</p-button>
+			<p-button :large="true">Large button</p-button>
+			<p-button :disabled="true">Disabled button</p-button>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {}
+import button from '../src';
+
+export default {
+	components: {
+		pButton: button,
+	}
+}
 </script>
 
 <style scoped lang="css"></style>

a4d1f3a37264cf3ac6eef3e4319b2b98a253d169
Thomas Cazade <cazade.thomas@gmail.com>
2017-12-04 11:41:14 +0100

    Added webpack and build command

---
+++ build/webpack.build.conf.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'papercss.js',
+    library: 'papercss-vue',
+    libraryTarget: 'umd',
+  },
+
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+  },
+
+  module: {
+    loaders: [
+      { test: /\.vue$/, loader: 'vue-loader' },
+      { test: /\.js$/, loader: 'babel-loader', include: __dirname, exclude: /node_modules/ }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize : true,
+      sourceMap : false,
+      mangle: true,
+      compress: { warnings: false },
+    }),
+  ],
+}

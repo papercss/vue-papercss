b99fc8d7576056540b688a1e7db280fcab02327f
Thomas Cazade <cazade.thomas@gmail.com>
2017-12-04 11:51:26 +0100

    Added a dev server environment

---
+++ build/webpack.demo.conf.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require("webpack");
+
+const modulesDir = path.join(__dirname, '..', 'node_modules');
+
+module.exports = {
+  entry: path.join(__dirname, '../demo/index.js'),
+
+  output: {
+    path: path.join(__dirname, '../demo'),
+    filename: 'papercss.bundle.js',
+  },
+
+  devServer: {
+    inline: true,
+    port: 8000,
+    contentBase: path.join(__dirname, '../demo'),
+  },
+
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'papercss-vue': path.join(__dirname, '..', 'src'),
+      'vue$': path.join(modulesDir, 'vue/dist/vue.common.js'),
+    },
+  },
+
+  resolveLoader: {
+    modules: [modulesDir],
+  },
+
+  module: {
+    loaders: [
+			{ test: /\.vue$/, loader: 'vue-loader' },
+      { test: /\.js$/, loader: 'babel-loader' },
+    ],
+  },
+}
---
+++ demo/App.vue
@@ -0,0 +1,9 @@
+<template lang="html">
+	<h1>PaperCSS for VueJS</h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="css"></style>

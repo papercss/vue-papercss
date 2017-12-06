750c9f04f2465161198b3adbdb22d35f63819df5
Thomas Cazade <cazade.thomas@gmail.com>
2017-12-06 20:43:10 +0100

    Added alert component

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import col from './components/col';
 import button from './components/button';
 import card from './components/card';
 import badge from './components/badge';
+import alert from './components/alert';
 
-export { container, row, col, button, card, badge };
+export { container, row, col, button, card, badge, alert };

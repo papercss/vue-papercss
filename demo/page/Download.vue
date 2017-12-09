1fff980e53e016b9fd8ba0d4f6119c1f066e5b74
Caleb Bassham <caleb.bassham@gmail.com>
2017-12-09 11:04:30 -0600

    Added a download tutorial page

---
+++ demo/page/Download.vue
@@ -0,0 +1,26 @@
+<template>
+  <p-container id="download">
+    <h2>Download</h2>
+    <h4>NPM</h4>
+    <p>PaperCSS For VueJS is not yet available on NPM.
+      <!--Install with <code>npm install vuejs-pappercss --save</code>--></p>
+    <h4>Yarn</h4>
+    <p>PaperCSS For VueJs is not yet available on NPM so it cannot be installed yet with Yarn.
+      <!--Install with <code>yarn add vuejs-pappercss</code>--></p>
+  </p-container>
+</template>
+
+<script>
+  import {container} from '../../src'
+
+  export default {
+    name: "download",
+    components: {
+      pContainer: container
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- demo/router.js
+++ demo/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './page/Home'
+import Download from './page/Download'
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/download',
+      name: 'download',
+      component: Download
     }
   ]
 })

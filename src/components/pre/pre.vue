a0b65b8eb58deb40b568d8f459c3791b43119b51
Caleb Bassham <caleb.bassham@gmail.com>
2017-12-09 12:18:01 -0600

    Added a pre component so that the pre styles can be used for code

---
+++ src/components/pre/pre.vue
@@ -0,0 +1,31 @@
+<template>
+  <slot></slot>
+</template>
+
+<script>
+    export default {
+        name: "pre"
+    }
+</script>
+
+<style scoped>
+  pre {
+    font-family: monospace, monospace;
+    font-size: 1em
+  }
+
+  pre {
+    display: block;
+    padding: 1em;
+    font-size: 80%;
+    line-height: 1.5;
+    color: #41403e;
+    word-break: break-all;
+    word-wrap: break-word;
+    background-color: #fafafa;
+    border: 1px solid #c1c0bd;
+    border-radius: 3px;
+    overflow-x: auto;
+    white-space: pre-wrap
+  }
+</style>

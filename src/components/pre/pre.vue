d74b0576a23a414627cfe79a29fa435048b1a517
DarkPaladin <caleb.bassham@gmail.com>
2017-12-09 12:59:08 -0600

    Update pre.vue

--- src/components/pre/pre.vue
+++ src/components/pre/pre.vue
@@ -8,24 +8,6 @@
     }
 </script>
 
-<style scoped>
-  pre {
-    font-family: monospace, monospace;
-    font-size: 1em
-  }
-
-  pre {
-    display: block;
-    padding: 1em;
-    font-size: 80%;
-    line-height: 1.5;
-    color: #41403e;
-    word-break: break-all;
-    word-wrap: break-word;
-    background-color: #fafafa;
-    border: 1px solid #c1c0bd;
-    border-radius: 3px;
-    overflow-x: auto;
-    white-space: pre-wrap
-  }
+<style scoped lang="css">
+  @import url('../../../node_modules/papercss/dist/components/code.css');
 </style>

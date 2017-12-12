1c0891d5921e168d3c4d9ab03dfe5d53afd37065
Caleb Bassham <caleb.bassham@gmail.com>
2017-12-12 06:35:33 -0600

    removed old tag when I was testing different methods for code blocks

--- demo/component/Demo.vue
+++ demo/component/Demo.vue
@@ -4,7 +4,6 @@
     <div ref="sourcecode" :id="'demo-' + title">
       <slot></slot>
     </div>
-    <p data-height="298" data-theme-id="0" data-slug-hash="aVePdm" data-default-tab="css,result" data-user="cbass1205" data-embed-version="2" data-pen-title="aVePdm" data-preview="true" class="codepen">See the Pen <a href="https://codepen.io/cbass1205/pen/aVePdm/">aVePdm</a> by Caleb Bassham (<a href="https://codepen.io/cbass1205">@cbass1205</a>) on <a href="https://codepen.io">CodePen</a>.</p>
   </div>
 </template>
 

88f70aec02327e4948d10cbc1c06de7a9635a00e
Caleb Bassham <caleb.bassham@gmail.com>
2017-12-13 17:07:29 -0600

    Added colored badges to demo

--- demo/page/component/Badge.vue
+++ demo/page/component/Badge.vue
@@ -1,7 +1,15 @@
 <template lang="html">
   <div>
-    <Demo title="Simple badge">
+    <Demo title="Simple Badge">
       <h4>Header <p-badge>5</p-badge></h4>
+    </Demo>
+
+    <Demo title="Colored Badges">
+      <h4>Header <p-badge color="primary">5</p-badge></h4>
+      <h4>Header <p-badge color="secondary">5</p-badge></h4>
+      <h4>Header <p-badge color="success">5</p-badge></h4>
+      <h4>Header <p-badge color="warning">5</p-badge></h4>
+      <h4>Header <p-badge color="danger">5</p-badge></h4>
     </Demo>
   </div>
 </template>

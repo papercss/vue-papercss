4c275da2de5b3f140a92f45c3cff153d22839299
Caleb Bassham <caleb.bassham@gmail.com>
2017-12-13 16:12:44 -0600

    Added demo for a simple badge

---
+++ demo/page/component/Badge.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+    <Demo title="Simple badge">
+      <h4>Header <p-badge>5</p-badge></h4>
+    </Demo>
+  </div>
+</template>
+
+<script>
+  import Demo from '../../component/Demo'
+  import { badge } from '../../../src'
+
+  export default {
+    name: "badge",
+    components: {
+      Demo,
+      pBadge: badge
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>

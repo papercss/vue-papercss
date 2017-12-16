749f21c8352f93e7fbdcc7e55e34fe4000758c11
Caleb Bassham <caleb.bassham@gmail.com>
2017-12-16 13:53:38 -0600

    Added a 404 page

---
+++ demo/page/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div style="text-align: center">
+    <h1 style="margin-bottom: 0;">404</h1>
+    <h4 style="margin: 0">There is not a page here!</h4>
+    <h4 style="margin: 0"><router-link to="/">How about going home?</router-link></h4>
+  </div>
+</template>
+
+<script>
+  import { button } from '../../src'
+
+  export default {
+    name: "not-found",
+    components: {
+      pButton: button
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>

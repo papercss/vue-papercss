e5dab6f34a50bddbcd67328c5700ad259f34539e
Thomas Cazade <cazade.thomas@gmail.com>
2018-12-09 18:44:14 +0100

    feat(index): create vue-papercss plugin

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import container from './components/container';
-import row from './components/row';
-import col from './components/col';
-import button from './components/button';
-import card from './components/card';
-import badge from './components/badge';
-import alert from './components/alert';
-import article from './components/article';
-import pre from './components/pre';
-import table from './components/table'
-import popover from './components/popover';
+import * as components from './components';
+import { vueUse } from './utils';
 
-export { container, row, col, button, card, badge, alert, article, pre, table, popover };
+const VuePaperCSSPlugin = {
+  install(Vue) {
+    if (Vue._papercss_vue_installed) {
+      return;
+    }
+
+    Vue._papercss_vue_installed = true;
+
+    /**
+     * Register all components into the Vue instance.
+     */
+    for (let plugin in components) {
+      Vue.use(components[plugin]);
+    }
+  }
+};
+
+/**
+ * Automatically install the plugin if there is `window.Vue` property.
+ */
+vueUse(VuePaperCSSPlugin);
+
+export default VuePaperCSSPlugin;

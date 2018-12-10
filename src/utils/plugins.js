e70bc1ed83d00b99417398d25ea75b0873036844
Thomas Cazade <cazade.thomas@gmail.com>
2018-12-10 10:03:44 +0100

    feat(plugins): register component util

--- src/utils/plugins.js
+++ src/utils/plugins.js
@@ -8,3 +8,24 @@ export function vueUse (VuePlugin) {
     window.Vue.use(VuePlugin)
   }
 }
+
+/**
+ * Register a component as a plugin being loaded, return true if the component
+ * is already registered.
+ *
+ * @param {object} Vue Vue constructor
+ * @param {string} name name of the component
+ * @param {object} definition component definition
+ */
+export function registerComponent(Vue, name, definition) {
+  Vue._papercss_vue_components = Vue._papercss_vue_components || {};
+
+  const loaded = Vue._papercss_vue_components[name];
+
+  if (!loaded && name && definition) {
+    Vue._papercss_vue_components[name] = true;
+    Vue.component(name, definition);
+  }
+
+  return loaded;
+}
